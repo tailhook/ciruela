65ed2afc20852460cde8181395f7460741a2394d
Paul Colomiets <paul@colomiets.name>
2016-12-24 02:08:07 +0200

    Add command-line params for daemon

--- src/daemon/main.rs
+++ src/daemon/main.rs
@@ -1,3 +1,31 @@
+extern crate argparse;
+extern crate env_logger;
+#[macro_use] extern crate log;
+
+use std::env;
+use std::path::PathBuf;
+
+use argparse::{ArgumentParser, Parse};
+
+
 fn main() {
-    unimplemented!();
+    if let Err(_) = env::var("RUST_LOG") {
+        env::set_var("RUST_LOG", "warn");
+    }
+    env_logger::init().unwrap();
+
+    let mut config_dir = PathBuf::from("/etc/ciruela");
+    let mut db_dir = PathBuf::from("/var/lib/ciruela");
+    {
+        let mut ap = ArgumentParser::new();
+        ap.refer(&mut config_dir)
+            .add_option(&["-c", "--config-base-dir"], Parse,
+                "A directory with configuration files (default /etc/ciruela)");
+        ap.refer(&mut db_dir)
+            .add_option(&["--db-dir"], Parse,
+                "A directory where to keep indexes of all directories and
+                 other files needed to operate server
+                 (default /var/lib/ciruela)");
+        ap.parse_args_or_exit();
+    }
 }
