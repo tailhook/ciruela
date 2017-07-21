ae80b45432cb5a4e6a9a93d297b92bc5d55dd759
Paul Colomiets <paul@colomiets.name>
2017-07-21 17:53:16 +0300

    Fixes publish-docs script

--- .travis/publish-docs.sh
+++ .travis/publish-docs.sh
@@ -2,8 +2,7 @@
 
 publish_docs() {
     pip install sphinx docutils ghp-import --user &&
-    pip install -r docs/requirements.txt --user &&
-    make html -C docs SPHINXBUILD=~/.local/bin/sphinx-build &&
-    ~/.local/bin/ghp-import -n docs/_build/html &&
+    make html -C doc SPHINXBUILD=~/.local/bin/sphinx-build &&
+    ~/.local/bin/ghp-import -n doc/_build/html &&
     git push -fq https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
 } && publish_docs
