6c18824ef5bdb705078469a9abf570751246d588
Paul Colomiets <paul@colomiets.name>
2018-04-22 00:26:23 +0300

    Better formatting of help

--- src/client/edit/mod.rs
+++ src/client/edit/mod.rs
@@ -21,91 +21,92 @@ use sync::convert_clusters;
     an equivalent image with an updated file back to the cluster.
 ")]
 pub struct EditOptions {
-    #[structopt(name="ENTRY_POINT", help="
-        Domain names used as entry points to a cluster. Only the first one
-        is used to download the file. Others are used to upload file back to
-        the cluster. Upload works the same as in `ciruela sync`.
+    #[structopt(name="ENTRY_POINT", help="\
+        Domain names used as entry points to a cluster. Only the first one \
+        is used to download the file. Others are used to upload file back to \
+        the cluster. Upload works the same as in `ciruela sync`. \
     ")]
     clusters: Vec<String>,
 
-    #[structopt(short="d", long="dir", help="
-        A virtual path to the directory to download file from.
+    #[structopt(short="d", long="dir", help="\
+        A virtual path to the directory to download file from. \
     ", parse(from_os_str))]
     dir: PathBuf,
 
-    #[structopt(short="f", long="file", help="
-        Path to file to edit, must start with backslash `/`.
+    #[structopt(short="f", long="file", help="\
+        Path to file to edit, must start with backslash `/`. \
     ", parse(from_os_str))]
     file: PathBuf,
 
-    #[structopt(long="editor", help="
-        Override editor to run. By default we're using EDITOR environment
-        variable.
+    #[structopt(long="editor", help="\
+        Override editor to run. By default we're using EDITOR environment \
+        variable. \
     ", parse(from_os_str))]
     editor: Option<PathBuf>,
 
-    #[structopt(short="m", long="multiple", help="
-        Multiple hosts per cluster mode.
-        See `ciruela sync --help` for more info on this mode.
+    #[structopt(short="m", long="multiple", help="\
+        Multiple hosts per cluster mode. \
+        See `ciruela sync --help` for more info on this mode. \
     ")]
     multiple: bool,
 
     #[structopt(short="i", long="identity", name="FILENAME",
                 raw(number_of_values="1"),
-                help="
-        Use the specified identity files (basically ssh-keys) to
-        sign the upload. By default all supported keys in
-        `$HOME/.ssh` and a key passed in environ variable `CIRUELA_KEY`
-        are used. Note: multiple `-i` flags may be used.
+                help="\
+        Use the specified identity files (basically ssh-keys) to \
+        sign the upload. By default all supported keys in \
+        `$HOME/.ssh` and a key passed in environ variable `CIRUELA_KEY` \
+        are used. Note: multiple `-i` flags may be used. \
     ")]
     identity: Vec<String>,
 
     #[structopt(short="k", long="key-from-env", name="ENV_VAR",
                 raw(number_of_values="1"),
-                help="
-        Use specified env variable to get identity (basically ssh-key).
-        The environment variable contains actual key, not the file
-        name. Multiple variables can be specified along with `-i`.
-        If neither `-i` nor `-k` options present, default ssh keys
-        and `CIRUELA_KEY` environment variable are used if present.
-        Useful for CI systems.
+                help="\
+        Use specified env variable to get identity (basically ssh-key). \
+        The environment variable contains actual key, not the file \
+        name. Multiple variables can be specified along with `-i`. \
+        If neither `-i` nor `-k` options present, default ssh keys \
+        and `CIRUELA_KEY` environment variable are used if present. \
+        Useful for CI systems. \
     ")]
     key_from_env: Vec<String>,
 
     #[structopt(short="e", long="early-timeout", name="EARLY_TIMEO",
                 parse(try_from_str="::humantime::parse_duration"),
                 default_value="30s",
-                help="
-        Report successful exit after this timeout even if not all hosts
-        received directories as long as most of them done
+                help="\
+        Report successful exit after this timeout even if not all hosts \
+        received directories as long as most of them done \
     ")]
     early_timeout: Duration,
 
     #[structopt(long="early-fraction", name="EARLY_FACTION",
                 default_value="0.75",
-                help="
-        Report successful exit after early timeout if this fraction if known
-        hosts are done.
+                help="\
+        Report successful exit after early timeout if this fraction if known \
+        hosts are done.\
     ")]
     early_fraction: f32,
 
     #[structopt(long="early-hosts", name="EARLY_HOSTS",
                 default_value="3",
-                help="
-        Report successful exit after early timeout after at least this number
-        of hosts are done, if this number is larger than
-        known-hosts*EARLY_FRACTION. If after early timeout number of known
-        hosts is less than this number the 100% of hosts are used as a measure.
+                help="\
+        Report successful exit after early timeout after at least this number \
+        of hosts are done, if this number is larger than \
+        known-hosts*EARLY_FRACTION. If after early timeout number of known \
+        hosts is less than this number the 100% of hosts are used as a \
+        measure. \
     ")]
     early_hosts: u32,
 
     #[structopt(short="t", long="deadline", name="DEADLINE",
                 parse(try_from_str="::humantime::parse_duration"),
                 default_value="30min",
-                help="
-        Maximum time ciruela sync is allowed to run. If not all hosts are
-        done and early exit conditions are not met utility will exit with
-        non-zero status.
+                help="\
+        Maximum time ciruela sync is allowed to run. If not all hosts are \
+        done and early exit conditions are not met utility will exit with \
+        non-zero status. \
     ")]
     deadline: Duration,
 }
--- src/client/sync/mod.rs
+++ src/client/sync/mod.rs
@@ -18,30 +18,30 @@ use global_options::GlobalOptions;
 
 
 #[derive(StructOpt, Debug)]
-#[structopt(name="ciruela sync", about="
-    A tool for bulk-uploading a set of directories to a set
-    of clusters *each* having a single name on a command-line
+#[structopt(name="ciruela sync", about="\
+    A tool for bulk-uploading a set of directories to a set \
+    of clusters *each* having a single name on a command-line \
     as an entry point (but see `-m`)
 
-    Executes a set of operations (uploads) to each mentioned
-    cluster. Cluster dns name (ENTRY_POINT) should resolve
-    to a multiple (e.g. at least three) ip addresses of
+    Executes a set of operations (uploads) to each mentioned \
+    cluster. Cluster dns name (ENTRY_POINT) should resolve \
+    to a multiple (e.g. at least three) ip addresses of \
     servers for reliability.
 
-    All uploading is done in parallel. Command
-    returns when all uploads are done or rejected.
+    All uploading is done in parallel. Command \
+    returns when all uploads are done or rejected. \
 ")]
 struct SyncOptions {
     #[structopt(name="ENTRY_POINT",
         help="Domain names used as entry points to a cluster")]
     clusters: Vec<String>,
 
-    #[structopt(short="m", long="multiple", help="
-        Multiple hosts per cluster mode. By default each hostname specified
-        on the command-line is a separate cluster (and is expected to be
-        resolved to multiple IP addresses). With this option set, hostnames
-        are treated as single cluster. You may still upload to a multiple
-        clusters by putting `--` at the start and use `--` as a separator.
+    #[structopt(short="m", long="multiple", help="\
+        Multiple hosts per cluster mode. By default each hostname specified \
+        on the command-line is a separate cluster (and is expected to be \
+        resolved to multiple IP addresses). With this option set, hostnames \
+        are treated as single cluster. You may still upload to a multiple \
+        clusters by putting `--` at the start and use `--` as a separator. \
     ")]
     multiple: bool,
 
@@ -65,61 +65,62 @@ struct SyncOptions {
 
     #[structopt(short="i", long="identity", name="FILENAME",
                 raw(number_of_values="1"),
-                help="
-        Use the specified identity files (basically ssh-keys) to
-        sign the upload. By default all supported keys in
-        `$HOME/.ssh` and a key passed in environ variable `CIRUELA_KEY`
-        are used. Note: multiple `-i` flags may be used.
+                help="\
+        Use the specified identity files (basically ssh-keys) to \
+        sign the upload. By default all supported keys in \
+        `$HOME/.ssh` and a key passed in environ variable `CIRUELA_KEY` \
+        are used. Note: multiple `-i` flags may be used. \
     ")]
     identity: Vec<String>,
 
     #[structopt(short="k", long="key-from-env", name="ENV_VAR",
                 raw(number_of_values="1"),
-                help="
-        Use specified env variable to get identity (basically ssh-key).
-        The environment variable contains actual key, not the file
-        name. Multiple variables can be specified along with `-i`.
-        If neither `-i` nor `-k` options present, default ssh keys
-        and `CIRUELA_KEY` environment variable are used if present.
-        Useful for CI systems.
+                help="\
+        Use specified env variable to get identity (basically ssh-key). \
+        The environment variable contains actual key, not the file \
+        name. Multiple variables can be specified along with `-i`. \
+        If neither `-i` nor `-k` options present, default ssh keys \
+        and `CIRUELA_KEY` environment variable are used if present. \
+        Useful for CI systems. \
     ")]
     key_from_env: Vec<String>,
 
     #[structopt(short="e", long="early-timeout", name="EARLY_TIMEO",
                 parse(try_from_str="::humantime::parse_duration"),
                 default_value="30s",
-                help="
-        Report successful exit after this timeout even if not all hosts
-        received directories as long as most of them done
-        (format: http://bit.ly/durationf).
+                help="\
+        Report successful exit after this timeout even if not all hosts \
+        received directories as long as most of them done \
+        (format: http://bit.ly/durationf). \
     ")]
     early_timeout: Duration,
 
     #[structopt(long="early-fraction", name="EARLY_FACTION",
                 default_value="0.75",
-                help="
-        Report successful exit after early timeout if this fraction if known
-        hosts are done.
+                help="\
+        Report successful exit after early timeout if this fraction if known \
+        hosts are done. \
     ")]
     early_fraction: f32,
 
     #[structopt(long="early-hosts", name="EARLY_HOSTS",
                 default_value="3",
-                help="
-        Report successful exit after early timeout after at least this number
-        of hosts are done, if this number is larger than
-        known-hosts*EARLY_FRACTION. If after early timeout number of known
-        hosts is less than this number the 100% of hosts are used as a measure.
+                help="\
+        Report successful exit after early timeout after at least this number \
+        of hosts are done, if this number is larger than \
+        known-hosts*EARLY_FRACTION. If after early timeout number of known \
+        hosts is less than this number the 100% of hosts are used as a \
+        measure. \
     ")]
     early_hosts: u32,
 
     #[structopt(short="t", long="deadline", name="DEADLINE",
                 parse(try_from_str="::humantime::parse_duration"),
                 default_value="30min",
-                help="
-        Maximum time ciruela sync is allowed to run. If not all hosts are
-        done and early exit conditions are not met utility will exit with
-        non-zero status (format: http://bit.ly/durationf).
+                help="\
+        Maximum time ciruela sync is allowed to run. If not all hosts are \
+        done and early exit conditions are not met utility will exit with \
+        non-zero status (format: http://bit.ly/durationf). \
     ")]
     deadline: Duration,
 }
